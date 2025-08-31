937b259bcb311d6dd381d39c6dcad82cde4741fb
The_Crowinator <136986967+TheCrowinator@users.noreply.github.com>
2025-08-31 06:00:08 +0100

    Add files via upload

---
+++ Source/ProjectHNAR/ProjectHNAR.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "ProjectHNAR.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, ProjectHNAR, "ProjectHNAR" );
